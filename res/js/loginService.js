00ce56e1f47b19c14727f5198059285949377cb1
Kaz <lamperouge1812@gmail.com>
2019-08-11 15:52:01 -0300

    Login completado

--- res/js/loginService.js
+++ res/js/loginService.js
@@ -12,3 +12,15 @@ function registrar(){
 function redirect(){
     window.location.href="registro.html";
 }
+
+function logear(){
+  var email = document.getElementById("txtUser").value;
+  var password=document.getElementById("txtPass").value;
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+  alert("Logeada");
+}
